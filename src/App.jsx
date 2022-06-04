df9c0b9717e8d51ce20d12486b8216dd538bf5e6
tishayaem <kostiantynkalinin@gmail.com>
2022-06-04 23:55:57 +0100

    Introduce state to the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 
 function Box() {
   const ref = useRef();
+  const { viewport } = useThree();
+
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+  });
   useFrame(() => {
-    ref.current.position.y += 0.01;
+    ref.current.position.set(0, (setData((data.y += 0.1)), 0));
+    if (data.y > viewport.heigt / 1.5) {
+      data.y = -viewport.heigt / 1.5;
+    }
   });
 
   return (
