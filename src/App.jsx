aa9c49261a5f61ed9d23f06f52a1cbb5f4761611
tishayaem <kostiantynkalinin@gmail.com>
2022-06-06 00:26:19 +0100

    Introduce the z axis

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 
-function Box() {
+function Box({z}) {
   const ref = useRef();
   const { viewport } = useThree();
 
@@ -11,7 +11,7 @@ function Box() {
     y: THREE.MathUtils.randFloatSpread(viewport.height),
   });
   useFrame(() => {
-    ref.current.position.set(data.x * viewport.width, (data.y += 0.1), 0);
+    ref.current.position.set(data.x * viewport.width, (data.y += 0.1), z);
 
     if (data.y > viewport.height / 1.5) {
       data.y = -viewport.height / 1.5;
@@ -30,7 +30,7 @@ function App({ count = 50 }) {
   return (
     <Canvas>
       {Array.from({ length: count }, (_, i) => (
-        <Box key={i} z={i} />
+        <Box key={i} z={-i} />
       ))}
     </Canvas>
   );
