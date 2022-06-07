7ab75bd305c27d2ec817dc698638c6c1dbac71ab
tishayaem <kostiantynkalinin@gmail.com>
2022-06-07 21:53:35 +0100

    Make boxes at the background appear from the bottom of the viewport

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import * as THREE from "three";
 import { useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 
-function Box({z}) {
+function Box({ z }) {
   const ref = useRef();
-  const { viewport } = useThree();
+  const { viewport, camera } = useThree();
+  const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
 
   const [data] = useState({
     x: THREE.MathUtils.randFloatSpread(2),
-    y: THREE.MathUtils.randFloatSpread(viewport.height),
+    y: THREE.MathUtils.randFloatSpread(height),
   });
   useFrame(() => {
-    ref.current.position.set(data.x * viewport.width, (data.y += 0.1), z);
+    ref.current.position.set(data.x * width, (data.y += 0.5), z);
 
-    if (data.y > viewport.height / 1.5) {
-      data.y = -viewport.height / 1.5;
+    if (data.y > height / 1.5) {
+      data.y = -height / 1.5;
     }
   });
 
